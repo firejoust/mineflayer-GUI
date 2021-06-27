9b3bb5ef79a9530ae1c9f6d8a0205c0b00ccb583
Firejoust <42525540+Camezza@users.noreply.github.com>
2021-06-27 14:19:41 +1000

    Add type definitions, refactor clickSlot and windowEvent methods

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const assert = require(`assert`);
 let Item, Window;
 
+/**
+ * @typedef {object} options
+ * @property {boolean?} hotbar If an item should be selected in the hotbar rather than the inventory
+ * @property {boolean?} rightclick Which mouse button to click with
+ * @property {number?} clickamount How many times to click the item
+ * @property {boolean?} shift If shift should be held whilst clicking
+ * @property {number?} delay How long to wait before clicking an item
+ * @property {number?} timeout How long to wait for a window to open
+ */
+
+/**
+ * @typedef {object} item
+ * @property {string?} display The displayed name of an item found by hovering over it with a cursor
+ * @property {string?} lore The lore of an item
+ * @property {string?} type The type of an item
+ * @property {number?} data The varying metadata of an item
+ * @property {number?} count How much of an item is present
+ * @property {options?} options
+ * 
+ */
+
 module.exports = init;
 
 function init(mineflayer) {
@@ -14,18 +35,14 @@ function inject(bot, options) {
 }
 
 class plugin {
-    /**
-     * Create an instance of plugin.
-     * @param {import('mineflayer').Bot} bot An instance of mineflayer.createBot
-     */
     constructor(bot) {
         this.bot = bot;
     }
 
     /**
-     * Retrieves a given item's display name in notchian format.
+     * Retrieves a given item's display name in notchian format
      * @param item Instance of a PrismarineItem
-     * @returns {string | null}
+     * @returns {string?}
      */
     getDisplay(item) {
         try {
@@ -43,7 +60,7 @@ class plugin {
     /**
      * Retrieves the lore of a given item as a string. Lores are seperated by a newline operator (\n).
      * @param item Instance of a PrismarineItem
-     * @returns {string | null}
+     * @returns {string?}
      */
     getLore(item) {
         try {
@@ -66,9 +83,9 @@ class plugin {
     }
 
     /**
-     * Retrieves an open window's slot according to a specified item.
-     * @param item CUSTOM
-     * @returns {number | null}
+     * Retrieves an open window's slot according to a specified item
+     * @param {item} item
+     * @returns {number?}
      */
     getSlot(item) {
         window = this.bot.currentWindow;
@@ -96,9 +113,70 @@ class plugin {
         return null;
     }
 
-    clickSlot();
-    clickHotbarSlot();
-    async windowEvent();
+    /**
+     * Clicks the specified item in the current window or inventory
+     * @param {number} slot
+     * @param {options} options
+     */
+    clickSlot(slot, options) {
+        let clicks = options.clickamount || 1;
+        let hotbar = options.hotbar || false;
+
+        if (hotbar) {
+            assert.ok(slot >= 36 && slot <= 45, `Cannot click slot that isn't present in hotbar`);
+
+            // Right click mode in hotbar is using an item
+            if (options.rightclick) {
+                for (let counter = 0; counter < clicks; counter++) {
+                    this.bot._client.write('use_item', {
+                        hand: slot === 45 ? 1 : 0, // use offhand for slot 45
+                    });
+                }
+                return;
+            }
+
+            // resume normal operation for left click
+            let click_position = this.bot.entity.position;
+            let click_block_face = 1;
+
+                for (let counter = 0; counter < clicks; counter++) {
+                    this.bot.swingArm();
+                    this.bot._client.write('block_dig', {
+                        status: 0,
+                        location: click_position,
+                        face: click_block_face,
+                    });
+                    this.bot._client.write('block_dig', {
+                        status: 1,
+                        location: click_position,
+                        face: click_block_face,
+                    });
+            }
+            return;
+        }
+
+        // click slot in window by default
+        for (let counter = 0; counter < clicks; counter++) {
+            this.bot.clickWindow(slot, options.rightclick ? 1 : 0, options.shift ? 1 : 0);
+        }
+    }
+    /**
+     * Waits for a window to open until the specified timeout.
+     * @param {options} options
+     * @return {void}  
+     */
+    async windowEvent(options) {
+        let handler, timeout;
+        return new Promise(function(resolve, reject) {
+            handler = resolve;
+            timeout = setTimeout(reject, options.timeout);
+            this.bot.once(`windowOpen`, handler);
+        }).finally(function() {
+            this.bot.removeListener(`windowOpen`, handler);
+            clearTimeout(timeout)
+        });
+    }
+
     async getWindow();
     async getItem();
     async clickItem();
